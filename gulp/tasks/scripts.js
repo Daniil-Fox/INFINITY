96057a6005f2bf71dc81bd9b5c10988b26cfacb9
Artyushenko Daniil <foxones80@gmail.com>
2025-11-07 00:37:26 +0600

    added profile page

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,43 +1,70 @@
-import browserSync from 'browser-sync';
-import webpackStream from 'webpack-stream';
-import plumber from 'gulp-plumber';
-import notify from 'gulp-notify';
+import browserSync from "browser-sync";
+import webpackStream from "webpack-stream";
+import plumber from "gulp-plumber";
+import notify from "gulp-notify";
+import path from "path";
+import fs from "fs";
 
 export const scripts = () => {
-  return app.gulp.src(app.paths.srcMainJs)
-    .pipe(plumber(
-      notify.onError({
-        title: "JS",
-        message: "Error: <%= error.message %>"
+  // Находим все файлы main*.js
+  const jsDir = path.resolve(`${app.paths.base.src}/js`);
+  const files = fs.readdirSync(jsDir);
+  const mainFiles = files
+    .filter((file) => file.startsWith("main") && file.endsWith(".js"))
+    .map((file) => path.join(jsDir, file));
+
+  // Создаем entry points для webpack
+  const entry = {};
+  mainFiles.forEach((file) => {
+    const name = path.basename(file, path.extname(file));
+    entry[name] = file;
+  });
+
+  return app.gulp
+    .src(`${app.paths.base.src}/js/main*.js`)
+    .pipe(
+      plumber(
+        notify.onError({
+          title: "JS",
+          message: "Error: <%= error.message %>",
+        })
+      )
+    )
+    .pipe(
+      webpackStream({
+        mode: app.isProd ? "production" : "development",
+        entry: entry,
+        output: {
+          filename: "[name].js",
+        },
+        module: {
+          rules: [
+            {
+              test: /\.m?js$/,
+              exclude: /node_modules/,
+              use: {
+                loader: "babel-loader",
+                options: {
+                  presets: [
+                    [
+                      "@babel/preset-env",
+                      {
+                        targets: "defaults",
+                      },
+                    ],
+                  ],
+                },
+              },
+            },
+          ],
+        },
+        devtool: !app.isProd ? "source-map" : false,
       })
-    ))
-    .pipe(webpackStream({
-      mode: app.isProd ? 'production' : 'development',
-      output: {
-        filename: 'main.js',
-      },
-      module: {
-        rules: [{
-          test: /\.m?js$/,
-          exclude: /node_modules/,
-          use: {
-            loader: 'babel-loader',
-            options: {
-              presets: [
-                ['@babel/preset-env', {
-                  targets: "defaults"
-                }]
-              ]
-            }
-          }
-        }]
-      },
-      devtool: !app.isProd ? 'source-map' : false
-    }))
-    .on('error', function (err) {
-      console.error('WEBPACK ERROR', err);
-      this.emit('end');
+    )
+    .on("error", function (err) {
+      console.error("WEBPACK ERROR", err);
+      this.emit("end");
     })
     .pipe(app.gulp.dest(app.paths.buildJsFolder))
     .pipe(browserSync.stream());
-}
+};
