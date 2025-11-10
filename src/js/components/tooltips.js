f24ac239b237059005e72b013a09c1e3dcdfc1ec
Artyushenko Daniil <foxones80@gmail.com>
2025-11-10 15:49:30 +0600

    added tooltip

--- src/js/components/tooltips.js
+++ src/js/components/tooltips.js
@@ -1,1 +1,15 @@
 import tippy from "tippy.js";
+
+window.addEventListener("DOMContentLoaded", () => {
+  const hints = document.querySelectorAll(".hint");
+
+  if (hints) {
+    hints.forEach((hint) => {
+      tippy(hint, {
+        content: hint.dataset.text,
+        theme: "tooltip",
+        arrow: false,
+      });
+    });
+  }
+});
